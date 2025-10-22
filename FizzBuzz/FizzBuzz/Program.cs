dd3600fe8dcf8367fdfbc198b047917c90c650a5
Therese Weidenstedt <kw222mi@student.lnu.se>
2025-10-22 15:32:51 +0200

    input

---
+++ FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Console.Write("Ange gränsen (t.ex. 100): ");
+            int limit = int.Parse(Console.ReadLine());
+
+            Console.Write("Ange första talet (X): ");
+            int x = int.Parse(Console.ReadLine());
+
+            Console.Write("Ange andra talet (Y): ");
+            int y = int.Parse(Console.ReadLine());
+
+
+        }
+        }
+    }
