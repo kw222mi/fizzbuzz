8002cd5a224002291a689b5dabcec42850fbcaf8
Therese Weidenstedt <kw222mi@student.lnu.se>
2025-10-23 03:50:57 +0200

    solution to R2

--- R2/R2/Program.cs
+++ R2/R2/Program.cs
@@ -16,6 +16,16 @@ namespace R2
             Console.WriteLine("- S:  the mean (average) of R1 and R2");
             Console.WriteLine("The program will calculate R2 so that (R1 + R2) / 2 = S.");
             Console.WriteLine();
+            Console.Write("Enter R1 and S: ");
+
+            string[] input = Console.ReadLine().Split(' ');
+            int R1 = int.Parse(input[0]);
+            int S = int.Parse(input[1]);
+
+            int R2 = 2 * S - R1;
+
+            Console.WriteLine();
+            Console.WriteLine($"The value of R2 is: {R2}");
 
         }
     }
