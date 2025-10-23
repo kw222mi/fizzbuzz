bbb1009295d66b259fea2180104a3ac89a91ae48
Therese Weidenstedt <kw222mi@student.lnu.se>
2025-10-23 14:38:02 +0200

    add reversed binary numbers

---
+++ ReversedBinaryNumbers/ReversedBinaryNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reversed_Binary_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Reversed Binary Numbers");
+            Console.WriteLine("Enter a number to reverse its binary representation:");
+
+            int input = int.Parse(Console.ReadLine());
+            string binary = Convert.ToString(input, 2);
+            string reversed = new string (binary.Reverse().ToArray());
+            int result = Convert.ToInt32(reversed, 2);
+            Console.WriteLine($"Original binary: {binary}");
+            Console.WriteLine($"Reversed binary: {reversed}");
+            Console.WriteLine($"Decimal result:  {result}");
+
+
+        }
+    }
+}
