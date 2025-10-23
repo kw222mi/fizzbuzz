4348cfed43fae73e72db96d4ad8b1a0794efd7fe
Therese Weidenstedt <kw222mi@student.lnu.se>
2025-10-23 13:20:17 +0200

    add solution planina

---
+++ Planina/Planina/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planina
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Planina – Calculate number of points");
+            Console.Write("Enter the number of iterations (N): ");
+
+            int n = int.Parse(Console.ReadLine());
+            int pointsPerSide = (int)Math.Pow(2, n) + 1;
+            int totalPoints = pointsPerSide * pointsPerSide;
+
+            Console.WriteLine($"Total number of points: {totalPoints}");
+        }
+    }
+}
